6af59db44318d866589696ba8ef3fe8579df05d5
rangelbruno <brunojp.rangel@gmail.com>
2026-02-01 17:13:29 -0300

    Feat(OnlyOffice): Configura OnlyOffice remoto com JWT
    - Altera URL padrão do OnlyOffice para https://onlyoffice.legisinc.com.br
    - Configura JWT para autenticação com o servidor OnlyOffice remoto
    - Remove serviço OnlyOffice local do docker-compose-dev-db.yml

--- sapl/settings.py
+++ sapl/settings.py
@@ -304,7 +304,7 @@ SERVER_EMAIL = config('SERVER_EMAIL', cast=str, default='')
 EMAIL_RUNNING = None
 
 # OnlyOffice Document Server Configuration
-ONLYOFFICE_URL = config('ONLYOFFICE_URL', default='http://localhost:8002')
+ONLYOFFICE_URL = config('ONLYOFFICE_URL', default='https://onlyoffice.legisinc.com.br')
 ONLYOFFICE_JWT_SECRET = config('ONLYOFFICE_JWT_SECRET', default='')
 ONLYOFFICE_JWT_ENABLED = config('ONLYOFFICE_JWT_ENABLED', cast=bool, default=False)
 
