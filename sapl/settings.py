4a73df61cf5457bbcf10662e456c22e5981612cf
rangelbruno <brunojp.rangel@gmail.com>
2026-02-01 17:31:44 -0300

    Fix(OnlyOffice): Aceita ONLYOFFICE_SERVER como fallback
    Adiciona compatibilidade para usar ONLYOFFICE_SERVER quando
    ONLYOFFICE_URL não estiver definido.

--- sapl/settings.py
+++ sapl/settings.py
@@ -304,7 +304,8 @@ SERVER_EMAIL = config('SERVER_EMAIL', cast=str, default='')
 EMAIL_RUNNING = None
 
 # OnlyOffice Document Server Configuration
-ONLYOFFICE_URL = config('ONLYOFFICE_URL', default='https://onlyoffice.legisinc.com.br')
+# Aceita ONLYOFFICE_URL ou ONLYOFFICE_SERVER (compatibilidade)
+ONLYOFFICE_URL = config('ONLYOFFICE_URL', default=config('ONLYOFFICE_SERVER', default='https://onlyoffice.legisinc.com.br'))
 ONLYOFFICE_JWT_SECRET = config('ONLYOFFICE_JWT_SECRET', default='')
 ONLYOFFICE_JWT_ENABLED = config('ONLYOFFICE_JWT_ENABLED', cast=bool, default=False)
 
